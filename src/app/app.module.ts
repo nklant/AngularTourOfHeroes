fe29fd6ee99d1f12235947e65b2b81547cf5a978
Nikolay Antonov <nikolay.antonov@globalcons.com>
2018-02-09 11:13:39 +0200

    test commit vs code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,31 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-
 import { HeroDetailComponent } from './hero-detail.component';
-
+import { HeroesComponent } from './heroes.component'
+import { HeroService } from './hero.service';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeroesComponent,
     HeroDetailComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      }
+    ])
   ],
-  providers: [],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+
+}
